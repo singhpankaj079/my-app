7783f38eff671f62bc2d7c14cd12e1592f832418
singhpankaj079 <spankaj2n@gmail.com>
2020-08-09 02:52:35 +0530

    Updates

---
+++ precache-manifest.834abcdb6705d5aa62c457ebdfc1eb47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fe8a89d3ccb9f111e3f59fcd90bfb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48187d6caac9d15e7f05",
+    "url": "/static/css/main.4a8fc8ec.chunk.css"
+  },
+  {
+    "revision": "ba34f672f863419192f7",
+    "url": "/static/js/2.db5a1371.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db5a1371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48187d6caac9d15e7f05",
+    "url": "/static/js/main.62863740.chunk.js"
+  },
+  {
+    "revision": "afd9fccc04eb1a18eb43",
+    "url": "/static/js/runtime-main.0c7a2f62.js"
+  }
+]);
